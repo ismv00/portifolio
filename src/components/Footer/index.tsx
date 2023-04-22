1fc85b89f86d39e46a9f72ddf7f5b26dded810e7
ismv00 <ismv00@icloud.com>
2023-04-22 01:28:49 -0400

    feat:footer component completed

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export function Footer() {
+  return (
+    <div className="container">
+      <div className="content-footer">
+        <p>© Igor S. Menezes. 2023 All rigths reserved</p>
+      </div>
+    </div>
+  );
+}
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,21 +4,23 @@ import "./index.css";
 export function Skills() {
   return (
     <div className="container">
-      <div className="content-skills">
-        <button>Portifolio</button>
-        <div>Skills</div>
-      </div>
+      <div data-aos="fade-up">
+        <div className="content-skills">
+          <button>Portifolio</button>
+          <div>Skills</div>
+        </div>
 
-      <div>
-        <ul className="content-grid">
-          {projects.map((item, index) => {
-            return (
-              <li key={index}>
-                <img src={item.image} alt="" />
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <ul className="content-grid">
+            {projects.map((item, index) => {
+              return (
+                <li key={index}>
+                  <img src={item.image} alt="" />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
